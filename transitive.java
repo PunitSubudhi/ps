c7385d9b106894d5705770067993573e32fdc1eb
PunitSubudhi <punitsubudhi@gmail.com>
2022-04-16 00:29:35 +0530

    idk

--- transitive.java
+++ transitive.java
@@ -7,28 +7,28 @@ public class transitive {
     private static double pow3(double num){
         return num*num*num;
     }
-
+    
     public static void main(String[] args) {
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.print("\na: ");
             double a = sc.nextDouble();
-
+            
             System.out.print("b: ");
             double b = sc.nextDouble();
-
+            
             System.out.print("c: ");
             double c = sc.nextDouble();
-
+            
             if(!(a<=pow3(b) && b<=pow3(c))) System.out.println("\nThe precondition is not met \n");
             else if(a<=pow3(b) && b<=pow3(c) && a<=pow3(c)){
                 System.out.println("\nThe relation is Transitive for the given example \n");
             } else System.out.println("\nThe relation is not Transitive \n");
-
+            
             System.out.println("a<=b^3: " + a + " <= " + pow3(b));
             System.out.println("b<=c^3: " + b + " <= " + pow3(c));
             System.out.println("a<=c^3: " + a + " <= " + pow3(c));
-
+            
             sc.close();
         }
     }
