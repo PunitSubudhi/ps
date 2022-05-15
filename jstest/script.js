346ce76467afe4a1ff851fb0a61577d18669eacb
PunitSubudhi <punitsubudhi@gmail.com>
2022-05-15 20:09:55 +0530

    v2

--- jstest/script.js
+++ jstest/script.js
@@ -1,35 +1,17 @@
-oddEven = true;
-function changeColou(){
-      document.getElementById('date').innerHTML = Date();
-      
-      if(oddEven){
-            document.body.style.backgroundColor = '#fff';
-            document.getElementById('date').style.color = '#000';
-            document.getElementById('colorButton').innerHTML = "Change color to black";
-            document.getElementById('colorButton').style.alignSelf = 'flex-end';
-            oddEven = false;
-      } else {
-            document.body.style.backgroundColor = '#000';
-            document.getElementById('date').style.color = '#fff';
-            document.getElementById('colorButton').innerHTML = "Change color to white";
-            document.getElementById('colorButton').style.alignSelf = 'flex-start';
-            oddEven = true;
-      }
+function updateValue(){
+    let val1 = document.getElementById('c1input').value;
+    document.getElementById('colorName1').innerHTML = val1;
+    document.getElementById('c1').style.backgroundColor = val1;
+
+    let val2 = document.getElementById('c2input').value;
+    document.getElementById('colorName2').innerHTML = val2;
+    document.getElementById('c2').style.backgroundColor = val2;
+
+    let val3 = document.getElementById('c3input').value;
+    document.getElementById('colorName3').innerHTML = val3;
+    document.getElementById('c3').style.backgroundColor = val3;
 }
 
-function setBodyColor(){
-     var bodColor =  document.getElementById('colorInput').value;
-     document.getElementById('colorDiv').style.backgroundColor = bodColor;
+function setDisplay(value,id){
+    document.getElementById(id).style.display = value;
 }
-
-function insertText(){
-      var text = document.getElementById('textInput').value;
-      document.getElementById('colorDivText').innerHTML = text;
-}
-
-function changeTextColor(){
-      var color = document.getElementById('textColorInput').value;
-      document.getElementById('colorDivText').style.color = color;
-}
-
-alert("This ia just a random website I made to showcase what ever I have learnt about JavaScript in the last 30 minutes, I have no Idea why I sent you this link, but anyway, now press ok and fiddle with it")
